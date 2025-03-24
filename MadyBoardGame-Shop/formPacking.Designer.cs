b506d35678452e15307f9cef2ca8d2845fa4c7a8
ThunpisitKrataichan <s6604022620114@email.kmutnb.ac.th>
2025-03-24 15:37:56 +0700

    fix : Everything jijgerbel

--- MadyBoardGame-Shop/formPacking.Designer.cs
+++ MadyBoardGame-Shop/formPacking.Designer.cs
@@ -32,19 +32,9 @@
             this.buttonBack = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.flowLayoutPackOrder = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBoxOrderdetail = new System.Windows.Forms.GroupBox();
             this.dataGridOrderDetails = new System.Windows.Forms.DataGridView();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.flowLayoutPackOrder.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.groupBoxOrderdetail.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridOrderDetails)).BeginInit();
             this.SuspendLayout();
@@ -73,7 +63,7 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.label1.Location = new System.Drawing.Point(300, 25);
+            this.label1.Location = new System.Drawing.Point(141, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(244, 75);
             this.label1.TabIndex = 0;
@@ -83,44 +73,10 @@
             // flowLayoutPackOrder
             // 
             this.flowLayoutPackOrder.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.flowLayoutPackOrder.Controls.Add(this.panel2);
             this.flowLayoutPackOrder.Location = new System.Drawing.Point(12, 123);
             this.flowLayoutPackOrder.Name = "flowLayoutPackOrder";
             this.flowLayoutPackOrder.Size = new System.Drawing.Size(417, 379);
             this.flowLayoutPackOrder.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(401, 80);
-            this.panel2.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "ออร์เดอร์ที่";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(298, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ยืนยันจัดส่ง";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // groupBoxOrderdetail
             // 
@@ -137,53 +93,8 @@
             this.dataGridOrderDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridOrderDetails.Location = new System.Drawing.Point(6, 28);
             this.dataGridOrderDetails.Name = "dataGridOrderDetails";
-            this.dataGridOrderDetails.Size = new System.Drawing.Size(613, 150);
+            this.dataGridOrderDetails.Size = new System.Drawing.Size(613, 281);
             this.dataGridOrderDetails.TabIndex = 0;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(90, 14);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 24);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "11";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 51);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 24);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "วันที่";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 51);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 24);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "22/03/2568";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(229, 51);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 24);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "สถานะ";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(280, 51);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(118, 24);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "ยืนยันการจัดส่ง";
             // 
             // formPacking
             // 
@@ -199,9 +110,6 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.formPacking_Load);
             this.panel1.ResumeLayout(false);
-            this.flowLayoutPackOrder.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.groupBoxOrderdetail.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridOrderDetails)).EndInit();
             this.ResumeLayout(false);
@@ -214,15 +122,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonBack;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPackOrder;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBoxOrderdetail;
         private System.Windows.Forms.DataGridView dataGridOrderDetails;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
     }
 }
--- MadyBoardGame-Shop/formPacking.cs
+++ MadyBoardGame-Shop/formPacking.cs
@@ -50,41 +50,48 @@ namespace MadyBoardGame_Shop
                 labelordertitle.Location = new Point(3, 15);
                 labelordertitle.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
                 labelordertitle.AutoSize = true;
+                labelordertitle.Click += Label_Click;
 
                 Label labelorder = new Label();
                 labelorder.Text = packingTable.Rows[i]["OrderID"].ToString();
                 labelorder.Location = new Point(90, 12);
                 labelorder.Font = new Font("Microsoft Sans Serif", 13, FontStyle.Regular);
                 labelordertitle.AutoSize = true;
+                labelorder.Click += Label_Click;
 
                 Label labelDatetitle = new Label();
                 labelDatetitle.Text = "วันที่ : ";
                 labelDatetitle.Location = new Point(3, 50);
                 labelDatetitle.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
                 labelDatetitle.AutoSize = true; 
+                labelDatetitle.Click += Label_Click;
 
                 Label labelDate = new Label();
                 labelDate.Text = packingTable.Rows[i]["PackDate"].ToString();
                 labelDate.Location = new Point(52, 47);
                 labelDate.Font = new Font("Microsoft Sans Serif", 13, FontStyle.Regular);
                 labelDate.AutoSize = true;
+                labelDate.Click += Label_Click;
 
                 Label labelstatusTitle = new Label();
                 labelstatusTitle.Text = "สถานะ : ";
-                labelstatusTitle.Location = new Point(230, 50);
+                labelstatusTitle.Location = new Point(225, 50);
                 labelstatusTitle.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
                 labelstatusTitle.AutoSize = true;
+                labelstatusTitle.Click += Label_Click;
 
                 Label labelstatus = new Label();
-                labelstatus.Text = packingTable.Rows[i]["PackStatus"].ToString();
-                labelstatus.Location = new Point(290, 50);
+                labelstatus.Text = packingTable.Rows[i]["PackStatus"].ToString()+ " ⏰";
+                labelstatus.Location = new Point(285, 50);
                 labelstatus.Font = new Font("Microsoft Sans Serif", 12, FontStyle.Regular);
                 labelstatus.AutoSize = true;
+                labelstatus.Click += Label_Click;
 
                 Button btnconfirm = new Button();
                 btnconfirm.Text = "ยืนยันจัดส่ง";
                 btnconfirm.Location = new Point(300, 12);
                 btnconfirm.Size = new Size(100, 34);
+                btnconfirm.Click += buttonConfiem_Click;
 
                 panel.Controls.Add(labelordertitle);
                 panel.Controls.Add(labelorder);
@@ -96,14 +103,57 @@ namespace MadyBoardGame_Shop
                 panel.Controls.Add(btnconfirm);
                 flowLayoutPackOrder.Controls.Add(panel);
             }
-
         }
 
+
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            Panel panel = (Panel)label.Parent;
+            string orderID = panel.Controls[1].Text;
+            using (SqlConnection orderdetailsConnection = new SqlConnection(InitializeUser._key_con))
+                {
+                    orderdetailsConnection.Open();
+                    string query = @"
+            SELECT 
+                OrderDetails.ProductID, 
+                Products.ProductName, 
+                OrderDetails.Quantity 
+            FROM 
+                OrderDetails 
+            INNER JOIN 
+                Products ON OrderDetails.ProductID = Products.ProductID 
+            INNER JOIN 
+                Orders ON OrderDetails.OrderID = Orders.OrderID 
+            WHERE 
+                OrderDetails.OrderID = @orderID
+            ORDER BY OrderDetails.ProductID;";
+
+                    using (SqlCommand orderdetailsCommand = new SqlCommand(query, orderdetailsConnection))
+                    {
+                        orderdetailsCommand.Parameters.AddWithValue("@orderID", orderID);
+                        SqlDataAdapter orderdetailsAdapter = new SqlDataAdapter(orderdetailsCommand);
+                        DataTable orderdetailsTable = new DataTable();
+                        orderdetailsAdapter.Fill(orderdetailsTable);
+
+                        // แสดงข้อมูลใน DataGridView
+                        dataGridOrderDetails.DataSource = orderdetailsTable;
+
+                        // กำหนดหัวคอลัมน์และความกว้างของคอลัมน์
+                        dataGridOrderDetails.Columns[0].HeaderText = "รหัสสินค้า";
+                        dataGridOrderDetails.Columns[1].HeaderText = "ชื่อสินค้า";
+                        dataGridOrderDetails.Columns[2].HeaderText = "จำนวน";
+                        dataGridOrderDetails.Columns[0].Width = 100;
+                        dataGridOrderDetails.Columns[1].Width = 390;
+                        dataGridOrderDetails.Columns[2].Width = 100;
+                    }
+            }
+        }
         private void Panel_Click(object sender, EventArgs e)
         {
             Panel panel = (Panel)sender;
             string orderID = panel.Controls[1].Text;
-            MessageBox.Show(orderID);
             using (SqlConnection orderdetailsConnection = new SqlConnection(InitializeUser._key_con))
             {
                 orderdetailsConnection.Open();
@@ -119,7 +169,8 @@ namespace MadyBoardGame_Shop
                 INNER JOIN 
                     Orders ON OrderDetails.OrderID = Orders.OrderID 
                 WHERE 
-                    OrderDetails.OrderID = @orderID;";
+                    OrderDetails.OrderID = @orderID
+                ORDER BY OrderDetails.ProductID;";
                 using (SqlCommand orderdetailsCommand = new SqlCommand(query, orderdetailsConnection))
                 {
                     orderdetailsCommand.Parameters.AddWithValue("@orderID", orderID);
@@ -135,6 +186,43 @@ namespace MadyBoardGame_Shop
                     dataGridOrderDetails.Columns[2].Width = 100;
                 }
             }
+        }
+        private void buttonConfiem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Label label = (Label)((Button)sender).Parent.Controls[5];
+                string orderID = ((Button)sender).Parent.Controls[1].Text;
+                using (SqlConnection updateConnection = new SqlConnection(InitializeUser._key_con))
+                {
+                    updateConnection.Open();
+                    string query = "UPDATE Packing SET PackStatus = @packStatus WHERE OrderID = @orderID";
+                    using (SqlCommand updateCommand = new SqlCommand(query, updateConnection))
+                    {
+                        updateCommand.Parameters.AddWithValue("@packStatus", "จัดเตรียมสำเร็จ");
+                        updateCommand.Parameters.AddWithValue("@orderID", orderID);
+                        updateCommand.ExecuteNonQuery();
+                    }
+                    string query2 = "UPDATE Products SET Quality = @quality";
+                    using (SqlCommand updateCommand2 = new SqlCommand(query2, updateConnection))
+                    {
+                        for (int i = 0; i < dataGridOrderDetails.Rows.Count; i++)
+                        {
+                            string productID = dataGridOrderDetails.Rows[i].Cells[0].Value.ToString();
+                            string quantity = dataGridOrderDetails.Rows[i].Cells[2].Value.ToString();
+                            updateCommand2.Parameters.AddWithValue("@quality", quantity);
+                            updateCommand2.ExecuteNonQuery();
+                        }
+                    }
+                }
+                ((Button)sender).Enabled = false;
+                label.Text = "จัดเตรียมสำเร็จ ✔️"; 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error : ",ex.Message);
+            }
+
         }
     }
 }
