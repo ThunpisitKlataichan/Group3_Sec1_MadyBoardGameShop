c0b44625bb9f646551ffe169b3c29f4912307fcf
ThunpisitKrataichan <s6604022620114@email.kmutnb.ac.th>
2025-03-29 12:48:21 +0700

    Test ให้แนทดู

--- MadyBoardGame-Shop/formCal.cs
+++ MadyBoardGame-Shop/formCal.cs
@@ -215,6 +215,7 @@ namespace MadyBoardGame_Shop
                         }
                     }
                 }
+                 
 
                 // Calculate and update total amount
                 if (quantityLabel != null && priceLabel != null)
