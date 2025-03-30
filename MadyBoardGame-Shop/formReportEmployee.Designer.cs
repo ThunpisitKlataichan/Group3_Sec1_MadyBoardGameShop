41cdee6c7904220111cc0f31b9fc6ec0bb1da01e
ThunpisitKrataichan <s6604022620114@email.kmutnb.ac.th>
2025-03-31 03:33:50 +0700

    Feat : formReportEmployee สำเร็จ

--- MadyBoardGame-Shop/formReportEmployee.Designer.cs
+++ MadyBoardGame-Shop/formReportEmployee.Designer.cs
@@ -30,37 +30,144 @@
         {
             this.dataGridResult = new System.Windows.Forms.DataGridView();
             this.btn_print_to_pdf = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxempID = new System.Windows.Forms.TextBox();
+            this.textBoxempName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridResult
             // 
             this.dataGridResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridResult.Location = new System.Drawing.Point(44, 207);
+            this.dataGridResult.Location = new System.Drawing.Point(8, 294);
+            this.dataGridResult.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dataGridResult.Name = "dataGridResult";
-            this.dataGridResult.Size = new System.Drawing.Size(683, 231);
+            this.dataGridResult.Size = new System.Drawing.Size(1314, 315);
             this.dataGridResult.TabIndex = 1;
             // 
             // btn_print_to_pdf
             // 
-            this.btn_print_to_pdf.Location = new System.Drawing.Point(585, 135);
+            this.btn_print_to_pdf.Location = new System.Drawing.Point(830, 146);
+            this.btn_print_to_pdf.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btn_print_to_pdf.Name = "btn_print_to_pdf";
-            this.btn_print_to_pdf.Size = new System.Drawing.Size(142, 57);
+            this.btn_print_to_pdf.Size = new System.Drawing.Size(146, 62);
             this.btn_print_to_pdf.TabIndex = 4;
             this.btn_print_to_pdf.Text = "print";
             this.btn_print_to_pdf.UseVisualStyleBackColor = true;
             this.btn_print_to_pdf.Click += new System.EventHandler(this.btn_print_to_pdf_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-9, -17);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1338, 130);
+            this.panel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonReset);
+            this.groupBox1.Controls.Add(this.buttonFind);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxempName);
+            this.groupBox1.Controls.Add(this.textBoxempID);
+            this.groupBox1.Location = new System.Drawing.Point(12, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(722, 150);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ค้นหา";
+            // 
+            // textBoxempID
+            // 
+            this.textBoxempID.Location = new System.Drawing.Point(150, 43);
+            this.textBoxempID.Name = "textBoxempID";
+            this.textBoxempID.Size = new System.Drawing.Size(123, 31);
+            this.textBoxempID.TabIndex = 0;
+            // 
+            // textBoxempName
+            // 
+            this.textBoxempName.Location = new System.Drawing.Point(401, 43);
+            this.textBoxempName.Name = "textBoxempName";
+            this.textBoxempName.Size = new System.Drawing.Size(315, 31);
+            this.textBoxempName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "รหัสพนักงาน :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "รายงานพนักงาน";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(279, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ชื่อพนักงาน :";
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(478, 97);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(116, 47);
+            this.buttonFind.TabIndex = 5;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(600, 97);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(116, 47);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
             // formReportEmployee
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1326, 624);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_print_to_pdf);
             this.Controls.Add(this.dataGridResult);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "formReportEmployee";
             this.Text = "formEmployeeReport";
+            this.Load += new System.EventHandler(this.formReportEmployee_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +176,14 @@
 
         private System.Windows.Forms.DataGridView dataGridResult;
         private System.Windows.Forms.Button btn_print_to_pdf;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxempName;
+        private System.Windows.Forms.TextBox textBoxempID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonReset;
     }
 }
--- MadyBoardGame-Shop/formReportEmployee.cs
+++ MadyBoardGame-Shop/formReportEmployee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Printing;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace MadyBoardGame_Shop
         {
             InitializeComponent();
         }
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataTable table;
+
         private int currentRowIndex = 0; // ใช้เก็บ index ของแถวที่พิมพ์ล่าสุด
         private void btn_print_to_pdf_Click(object sender, EventArgs e)
         {
@@ -85,6 +91,123 @@ namespace MadyBoardGame_Shop
                 e.HasMorePages = false;
                 currentRowIndex = 0; // รีเซ็ตค่า
             }
+        }
+
+        private void formReportEmployee_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadIntoDataGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadIntoDataGrid()
+        {
+            try
+            {
+                connection = new SqlConnection(InitializeUser._key_con);
+                connection.Open();
+
+                // แก้ไข query ให้ตรงกับชื่อคอลัมน์จริงในฐานข้อมูล
+                string query = "SELECT empID, empName, empPosition, empEmail, empSalary FROM Employees WHERE 1=1";
+
+                // เพิ่มเงื่อนไขการค้นหาตามที่ระบุในฟอร์ม
+                if (!string.IsNullOrEmpty(textBoxempID.Text))
+                {
+                    query += " AND empID LIKE @EmployeeID";
+                }
+
+                if (!string.IsNullOrEmpty(textBoxempName.Text))
+                {
+                    query += " AND empName LIKE @EmployeeName";
+                }
+
+                command = new SqlCommand(query, connection);
+
+                // เพิ่ม parameters เพื่อป้องกัน SQL Injection
+                if (!string.IsNullOrEmpty(textBoxempID.Text))
+                {
+                    command.Parameters.AddWithValue("@EmployeeID", "%" + textBoxempID.Text + "%");
+                }
+
+                if (!string.IsNullOrEmpty(textBoxempName.Text))
+                {
+                    command.Parameters.AddWithValue("@EmployeeName", "%" + textBoxempName.Text + "%");
+                }
+
+                adapter = new SqlDataAdapter(command);
+                table = new DataTable();
+                adapter.Fill(table);
+
+                dataGridResult.DataSource = table;
+
+                // ปรับแต่งการแสดงผลใน DataGridView
+                CustomizeDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("เกิดข้อผิดพลาดในการโหลดข้อมูล: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void CustomizeDataGridView()
+        {
+            // จัดรูปแบบคอลัมน์ต่างๆ
+            if (dataGridResult.Columns.Contains("empSalary"))
+            {
+                dataGridResult.Columns["empSalary"].DefaultCellStyle.Format = "N2";
+            }
+
+            // ตั้งค่าความกว้างคอลัมน์อัตโนมัติ
+            dataGridResult.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            // ตั้งชื่อหัวคอลัมน์ให้อ่านง่าย (ใช้ชื่อคอลัมน์ตามที่ query มา)
+            if (dataGridResult.Columns.Contains("empID"))
+            {
+                dataGridResult.Columns["empID"].HeaderText = "รหัสพนักงาน";
+            }
+            if (dataGridResult.Columns.Contains("empName"))
+            {
+                dataGridResult.Columns["empName"].HeaderText = "ชื่อพนักงาน";
+            }
+            if (dataGridResult.Columns.Contains("empPosition"))
+            {
+                dataGridResult.Columns["empPosition"].HeaderText = "ตำแหน่ง";
+            }
+            if (dataGridResult.Columns.Contains("empEmail"))
+            {
+                dataGridResult.Columns["empEmail"].HeaderText = "อีเมล";
+            }
+            if (dataGridResult.Columns.Contains("empSalary"))
+            {
+                dataGridResult.Columns["empSalary"].HeaderText = "เงินเดือน";
+            }
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            LoadIntoDataGrid();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            // ล้างข้อมูลใน TextBox ทั้งหมด
+            textBoxempID.Text = "";
+            textBoxempName.Text = "";
+
+            // โหลดข้อมูลทั้งหมดใหม่
+            LoadIntoDataGrid();
         }
     }
 }
